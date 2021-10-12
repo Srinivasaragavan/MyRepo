bf707a15f9d36bc7dcf24d4b1ad7fccdd0d211d0
Srinivasaragavan <srinimuniappan@yahoo.co.in>
2021-10-12 23:16:29 +0530

    stage 01

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,10 @@
+from src.utils.all_utils import read_config
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+    args = argparse.ArguementParser()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+    print(parsed_args.config)
