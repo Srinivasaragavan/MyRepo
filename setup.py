bff9f4cf4d67a05ce570eeb25846ea1fb30785cd
Srinivasaragavan <srinimuniappan@yahoo.co.in>
2021-10-13 16:04:45 +0530

    commited

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md","r",encoding="utf-8") as f:
     long_description =f.read()
 
 setup(
-    name="src"
-    version="0.0.1"
-    author="srinivasaragavan"
-    description="A small package for dvc ml pipeline demo"
-    long_description=long_description
-    long_description_content_type="text/markdown"
+    name="src",
+    version="0.0.1",
+    author="srinivasaragavan",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Srinivasaragavan/myrepodvc.git",
-    author_email="srinimuniappan@yahoo.co.in"
-    packages=["src"]
-    python_requires=">=3.7"
+    author_email="srinimuniappan@yahoo.co.in",
+    packages=["src"],
+    python_requires=">=3.7",
     install_requires=[
         'dvc'
         'pandas'
